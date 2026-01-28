83e95191f2faf2802314cff8382f5aee8370a29c
tethys2 <tethys2@users.noreply.github.com>
2026-01-28 05:59:14 +0000

    deploy: aa80f2960011b571c9f639aa6845d61b8ed0f2ba

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,8 +2,10 @@ var searchData=
 [
   ['handlebuttonpress_0',['handleButtonPress',['../events_8c.html#a6163226ea35fd631cb3240dd1f68566b',1,'events.c']]],
   ['handlebuttonrelease_1',['handleButtonRelease',['../events_8c.html#aa51e79f4ea49835f825b929b12238fe0',1,'events.c']]],
-  ['handleevent_2',['handleEvent',['../events_8c.html#a56982b0acb3b449e10b6728b13b3d162',1,'handleEvent(xcb_generic_event_t *ev):&#160;events.c'],['../events_8h.html#a56982b0acb3b449e10b6728b13b3d162',1,'handleEvent(xcb_generic_event_t *ev):&#160;events.c']]],
-  ['handlekeypress_3',['handleKeyPress',['../events_8c.html#a57175c42f21412727c7a4222999e4b49',1,'events.c']]],
-  ['handlemapnotify_4',['handleMapNotify',['../events_8c.html#a875581a1c3b57782ae3e8bafbc7866b6',1,'events.c']]],
-  ['handlemaprequest_5',['handleMapRequest',['../events_8c.html#a2ba5f74c7bc352faced360ccb95ae597',1,'events.c']]]
+  ['handleenternotify_2',['handleEnterNotify',['../events_8c.html#a3ebd5d73389ae065c4fccf6607652c64',1,'events.c']]],
+  ['handleevent_3',['handleEvent',['../events_8c.html#a56982b0acb3b449e10b6728b13b3d162',1,'handleEvent(xcb_generic_event_t *ev):&#160;events.c'],['../events_8h.html#a56982b0acb3b449e10b6728b13b3d162',1,'handleEvent(xcb_generic_event_t *ev):&#160;events.c']]],
+  ['handlekeypress_4',['handleKeyPress',['../events_8c.html#a57175c42f21412727c7a4222999e4b49',1,'events.c']]],
+  ['handlemapnotify_5',['handleMapNotify',['../events_8c.html#a875581a1c3b57782ae3e8bafbc7866b6',1,'events.c']]],
+  ['handlemaprequest_6',['handleMapRequest',['../events_8c.html#a2ba5f74c7bc352faced360ccb95ae597',1,'events.c']]],
+  ['handlemotionnotify_7',['handleMotionNotify',['../events_8c.html#aabd74beb09b6c964532d09af1f122256',1,'events.c']]]
 ];
